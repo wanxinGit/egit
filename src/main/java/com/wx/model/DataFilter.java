53758fd43ca22c18a9b5269e613766cf1cbe75a8
wanxin <258621580@qq.com>
2019-01-24 16:49:07 +0800

    ide中提交一个修改

--- src/main/java/com/wx/model/DataFilter.java
+++ src/main/java/com/wx/model/DataFilter.java
@@ -32,13 +32,13 @@ public @interface DataFilter {
     String tableAlias() default "";
 
     /**  true：没有本部门数据权限，也能查询本人数据 */
-    boolean user1() default true;
+    boolean user111() default true;
 
     /**  true：拥有子部门数据权限1111111 */
     boolean subDept() default false;
 
     /**  部门ID */
-    String deptId() default "dept_id";
+    String deptId333() default "dept_id";
 
     /**  用户ID */
     String userId() default "user_id";
