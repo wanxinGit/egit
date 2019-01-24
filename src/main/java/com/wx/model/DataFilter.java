22a127fa1483ef4c9dcf1462480d06f2782ad1fa
wanxin <258621580@qq.com>
2019-01-24 16:35:00 +0800

    web端提交的一个修改，测试冲突。
    web端提交的一个修改，测试冲突。

--- src/main/java/com/wx/model/DataFilter.java
+++ src/main/java/com/wx/model/DataFilter.java
@@ -32,9 +32,9 @@ public @interface DataFilter {
     String tableAlias() default "";
 
     /**  true：没有本部门数据权限，也能查询本人数据 */
-    boolean user() default true;
+    boolean user1() default true;
 
-    /**  true：拥有子部门数据权限 */
+    /**  true：拥有子部门数据权限1111111 */
     boolean subDept() default false;
 
     /**  部门ID */
