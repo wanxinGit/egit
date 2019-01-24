a6ebab9601a892188638ff11e3737f07d8cb2867
wanxin <258621580@qq.com>
2019-01-24 17:10:42 +0800

    web端修改，测试冲突
    web端修改，测试冲突

--- src/main/java/com/wx/model/DataFilter.java
+++ src/main/java/com/wx/model/DataFilter.java
@@ -32,13 +32,13 @@ public @interface DataFilter {
     String tableAlias() default "";
 
     /**  true：没有本部门数据权限，也能查询本人数据 */
-    boolean user1112() default true;
+    boolean user1112444() default true;
 
     /**  true：拥有子部门数据权限1111111222222 */
     boolean subDept() default false;
 
     /**  部门ID */
-    String deptId333() default "dept_id";
+    String deptId3335555() default "dept_id";
 
     /**  用户ID */
     String userId() default "user_id";
